679b368e270eace0873bd7081e6d91d73e702bfa
Erwin Nahue <erwinnahue@gmail.com>
2022-06-04 18:27:52 -0500

    Agregar archivos de proyecto.

---
+++ Consultadatos/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace Consultadatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txb_rdni = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(86, 12);
+            this.textBox1.MaxLength = 8;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txb_rdni
+            // 
+            this.txb_rdni.Location = new System.Drawing.Point(86, 38);
+            this.txb_rdni.Name = "txb_rdni";
+            this.txb_rdni.ReadOnly = true;
+            this.txb_rdni.Size = new System.Drawing.Size(224, 20);
+            this.txb_rdni.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "DNI";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(4, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Nombres y Ap.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 80);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txb_rdni);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Consultar DNI";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txb_rdni;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Consultadatos/Form1.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Consultadatos.apiRest;
+
+
+
+
+
+namespace Consultadatos
+{
+    public partial class Form1 : Form
+    {
+        apidb dBApi = new apidb();
+        
+        public Form1()
+        {
+            InitializeComponent();
+            
+
+        }
+
+        
+        public  void rconsulta()
+        {
+            txb_rdni.Text = "";
+            
+            try
+            {
+                
+                dynamic respuesta = dBApi.Get("https://api.apis.net.pe/v1/dni?numero=" + textBox1.Text + "");
+                txb_rdni.Text = respuesta.nombre.ToString();
+
+            }
+            catch
+            {
+                MessageBox.Show("DNI no existe");
+                
+                textBox1.Text = "";
+                textBox1.Focus();
+            }
+        }
+
+               
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (this.textBox1.Text.Length == 8)
+            { this.txb_rdni.Focus();
+                rconsulta();
+
+            }
+
+           
+        }
+    }
+}
